2c8a49c8e97f61f13bf1356f4329a03781e5d17a
vincecalayag <85989795+vincecalayag@users.noreply.github.com>
2021-06-17 17:50:59 +0800

    Add files via upload

---
+++ PWAM/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener("install", e => { 
+    e.waitUntil(
+      caches.open("offlineFiles").then(cache => {
+         return cache.addAll([
+             "./",
+             "./src/index.js",
+             "./src/styles.css",
+             "./images/192.png",
+             "./images/512.png"
+         ]);
+      })
+    );
+});
+
+self.addEventListener("fetch", e => {
+   console.log("Intercepting request :"+ e.request.url);
+   e.respondWith(
+       caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+       })
+   );
+});
